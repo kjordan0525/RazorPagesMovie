4ff8891063efdc4a49d2ec12710a41e29237a1aa
Kyle Jordan <kjordan0525@gmail.com>
2020-01-10 23:02:35 -0800

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
